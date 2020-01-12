4c2f582df785584b78225ad5afa1dca60808a354
rmashhadi <rme1383@gmail.com>
2020-01-12 13:08:53 +0330

    Updates

---
+++ precache-manifest.61c561e42635a7eb687070f4e306e114.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055a07270f98e081bd994aab5e0059d0",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "dfbd9825d8d82296e904",
+    "url": "/deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "475c21ca2b81859b2665",
+    "url": "/deploy/static/js/2.2d90dc5e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/deploy/static/js/2.2d90dc5e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfbd9825d8d82296e904",
+    "url": "/deploy/static/js/main.ab000835.chunk.js"
+  },
+  {
+    "revision": "f107d2bb054cffd514fb",
+    "url": "/deploy/static/js/runtime-main.6403fb6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
